47286b92ab9564ad1dca6468282981df6bca2e83
Diego Herrera Gre <diego.herrera.94@outlook.com>
2024-02-23 17:56:42 -0300

    FE DE ERRATAS:
    - Faltó agregar archivo en componente

---
+++ src/components/Cart.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../App.css';
+import Books from './Books';
+
+async function getCart() {
+    await const goToBasket = () => {
+        window.location.href = '/cart';
+    }
+}
