ee3cb7ea25f4dd95e726d597f8895ce4d40b5bbb
Diego Herrera Gre <diego.herrera.94@outlook.com>
2024-02-25 13:35:54 -0300

    Modificiaciones 25/02/2024
    - Instalación de Webpack-image-loader
    - Creación de webpack.config.js
    - Vinculación de imágenes de portadas de libros con el ID trabajado.

--- src/components/BookId.jsx
+++ src/components/BookId.jsx
@@ -1,17 +1,25 @@
 import { useParams } from 'react-router-dom';
-import books2024 from '../data';
+import { Link } from 'react-router-dom';
+import books2024  from '../data';
+import bookImage from '../bookImage';
 import '../App.css'
 function BookId() {
     const {id} = useParams();
     const book = books2024.find(book => book.id === Number(id));
+    const bookId = bookImage[id];
+
     return (
         <article id='books-desc-main' key={id}>
+            <div>
+                <img src={bookId} alt={book ? book.title : 'No se ha encontrado el libro'}/>
+            </div>
             <h3>{book ? book.title : 'No se ha encontrado el libro'}</h3>
             <p id='effect-shake'>{book ? book.year : ''}</p>
             <div id='p-container'>
                 <p className='p-working-css'>{book ? book.editorial : ''}</p>
                 <p className='p-working-css'>{book ? book.price : ''}</p>
-                <p className='p-working-css'>{book ? book.source : ''}</p>
+                <Link to={book ? book.source : ''}><span className='btn-buying'>COMPRE AQUÍ</span></Link>
+                <p className='p-working-css'>{book ? book.description : ''}</p>
             </div>
         </article>
     );
